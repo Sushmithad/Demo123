432b55172257077ac0e4d5ffeb8af8f9b64c843a
H2_9_19_User4 <H2_9_19_User4@172.31.37.149>
2020-02-12 06:18:02 +0000

    review level1

--- annot/src/main/java/annot/annot/Tst.java
+++ annot/src/main/java/annot/annot/Tst.java
@@ -5,6 +5,7 @@ public class Tst {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("code by srini");
+System.out.println("code review by uday kiran");
 	}
 
 }
